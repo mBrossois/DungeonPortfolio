d7bece7e13aa7e6fd43a24bcc3b42a56c0c52f4c
Mark de Neut Brossois <mark.dnb@live.nl>
2023-12-10 15:30:23 +0100

    added animations

--- src/components/icons/HamburgerIcon.vue
+++ src/components/icons/HamburgerIcon.vue
@@ -1,6 +1,6 @@
 <template>
     <svg width="46" height="57" viewBox="0 0 46 57" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g clip-path="url(#clip0_43_77)">
+        <g :class="{'open': isMenuOpen}" clip-path="url(#clip0_43_77)">
             <rect y="1.52588e-05" width="46" height="13.7544" rx="2" fill="#60584C"/>
             <rect y="20.6316" width="46" height="13.7544" rx="2" fill="#60584C"/>
             <rect y="43.2281" width="46" height="13.7544" rx="2" fill="#60584C"/>
@@ -14,25 +14,57 @@
 
 </template>
 
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  isMenuOpen: Boolean
+})
+</script>
+
 <style scoped>
     svg {
         cursor: pointer;
     }
 
     rect {
-        transition: transform 200ms ease-in;
+        transition: all 200ms ease-in;
+        fill: #60584C;
+        transform-box: fill-box;
+        transform-origin: center;
     }
 
     g > rect:nth-child(1) {
-        transition: transform 300ms ease-in;
+        transition: all 300ms ease-in;
     }
 
-    svg:hover > g > rect:nth-child(1) {
+    svg:hover > g:not(.open) > rect:nth-child(1) {
         transform: translateY(16px);
     }
 
-    svg:hover > g > rect:nth-child(2) {
+    svg:hover > g:not(.open) > rect:nth-child(2) {
         transform: translateY(9px);
     }
+
+    svg > g.open > rect:nth-child(1) {
+        fill: #60584C00;
+    }
+
+    svg > g.open > rect:nth-child(2) {
+        transform: rotate(45deg);
+    }
+
+    svg > g.open > rect:nth-child(3) {
+        transform: translateY(-22px) rotate(-45deg);
+    }
+
+    svg:hover > g.open > rect:nth-child(2) {
+        transform: rotate(50deg);
+    }
+
+    svg:hover > g.open > rect:nth-child(3) {
+        transform: translateY(-22px) rotate(-50deg);
+    }
+
 
 </style>
